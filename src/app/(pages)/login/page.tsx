3a710cba9705bbdd0a56d35362c7cb49c836a171
cp3hnu <cp3hnu@gmail.com>
2025-06-21 08:44:15 +0800

    feat: 添加 placeholder

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -25,11 +25,11 @@ export default function LoginPage() {
         <form className="space-y-6" action={formAction}>
           <div>
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">用户名</label>
-            <input type="text" id="username" name="username" required className="input-base" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" id="username" name="username" required className="input-base" placeholder="请输入用户名" value={username} onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div>
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">密码</label>
-            <input type="password" id="password" name="password" required className="input-base" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" id="password" name="password" required className="input-base" placeholder="请输入密码" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className="flex items-center justify-between space-x-4">
             <button type="submit" className="primary-button" disabled={isPending}>
